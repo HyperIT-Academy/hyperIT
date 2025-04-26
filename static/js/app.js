14a9d244bc0319f17550de13469679018eec9472
HyperIT-Academy <hyperitacademy@gmail.com>
2025-04-26 20:15:50 +0300

    HyperIT site: Part 1.

---
+++ static/js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  $(".reviews-carousel").slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  arrows: true,
+                  slidesToShow: 1,
+                  centerMode: true,
+                  centerPadding: '50px'
+              },
+          }
+      ],
+  });
+
+
+  $(".faq-btn-js").on("click", function () {
+      var $answer = $(this).closest("li").find(".faq__answer");
+      $answer.toggleClass("hidden");
+      $(this).toggleClass("rotate");
+  });
+
+
+  const $input = $(".hero-input-phone");
+
+  $input.intlTelInput({
+    initialCountry: "auto",
+    locale: "en",
+    localizedCountries: {
+      cg: "Congo - Brazzaville",
+      cd: "Congo - Kinshasa"
+    },
+    nationalMode: true,
+    autoPlaceholder: "polite",
+    formatOnDisplay: false,
+    separateDialCode: false, 
+    geoIpLookup: function (callback) {
+      fetch("https://ipinfo.io/json?token=9f9dee509d49e4")
+        .then(res => res.json())
+        .then(data => callback(data.country))
+        .catch(() => callback("ua"));
+    },
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
+  });
+
+
+  setTimeout(() => {
+      document.querySelectorAll(".iti__country-name").forEach(el => {
+        el.textContent = el.textContent.replace(/\s*\(.*?\)/, "");
+      });
+  }, 500);
+});
---
+++ static/js/modal.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+
+    $(".free-lesson-btn-js").on("click", function() {
+      $(".backdrop").removeClass("is-hidden");
+    });
+
+
+    $(".modal__close-btn").on("click", function() {
+      $(".backdrop").addClass("is-hidden");
+      $(".error").text("");
+      $(".form input").removeClass("error is-invalid");
+    });
+
+    $(".form").on("submit", function (e) {
+        e.preventDefault();
+      
+        let isValid = true;
+      
+        const form = $(this);
+        const name = form.find(".hero-input-name").val().trim();
+        const phone = form.find(".hero-input-phone").val().trim();
+        const email = form.find(".hero-input-email").val().trim();
+        const privacy = form.find(".hero-input-privacy").is(":checked");
+      
+        form.find(".error").text("");
+        form.find("input").removeClass("error");
+      
+        if (name === "") {
+          form.find(".error-name").text("Ім’я обовʼязкове");
+          form.find(".hero-input-name").addClass("error");
+          isValid = false;
+        }
+      
+        if (!/^\+?[0-9\s\-\(\)]{10,20}$/.test(phone)) {
+          form.find(".error-phone").text("Номер телефону невірний!");
+          form.find(".hero-input-phone").addClass("error");
+          isValid = false;
+        }
+      
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+          form.find(".error-email").text("Email обовʼязковий і має бути валідним");
+          form.find(".hero-input-email").addClass("error");
+          isValid = false;
+        }
+      
+        if (!privacy) {
+          form.find(".error-privacy").text("Потрібно погодитися з умовами");
+          form.find(".checkbox").addClass("is-invalid");
+          isValid = false;
+        }
+      
+        if (isValid) {
+          alert("Дані валідні! Надсилаємо...");
+
+          $.ajax({
+            method: "POST",
+            url: "/send",
+            contentType: "application/json",
+            data: JSON.stringify({ name, phone, email }),
+            success: function (res) {
+                alert("Лист успішно надіслано!");
+                $(".form")[0].reset();
+            },
+            error: function () {
+                alert("Щось пішло не так. Спробуйте ще раз.");
+            }
+          });
+        }
+    });
+
+    $(".form input").on("input", function () {
+        const $input = $(this);
+        const value = $input.val().trim();
+        const name = $input.attr("name");
+      
+        if (name === "name" && value !== "") {
+          $input.removeClass("error");
+          $(".error-name").text("");
+        }
+      
+        if (name === "phone" && /^\+?[0-9\s\-\(\)]{10,20}$/.test(value)) {
+          $input.removeClass("error");
+          $(".error-phone").text("");
+        }
+      
+        if (name === "email" && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+          $input.removeClass("error");
+          $(".error-email").text("");
+        }
+    });
+
+    $(".form input[type='checkbox']").on("change", function () {
+        if ($(this).is(":checked")) {
+          $(".checkbox").removeClass("is-invalid");
+          $(".error-privacy").text("");
+        }
+    });
+});
