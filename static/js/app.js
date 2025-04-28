c11bd02f9bd62977339558b2399c2341ce2a9057
HyperIT-Academy <hyperitacademy@gmail.com>
2025-04-28 19:24:47 +0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,3 @@
-export async function initPhoneInput() {
-  const $input = $(".hero-input-phone");
-
-  await $input.intlTelInput({
-    initialCountry: "auto",
-    locale: "en",
-    localizedCountries: {
-      cg: "Congo - Brazzaville",
-      cd: "Congo - Kinshasa"
-    },
-    nationalMode: true,
-    autoPlaceholder: "polite",
-    formatOnDisplay: false,
-    separateDialCode: true, 
-    geoIpLookup: async function (callback) {
-      try {
-        const res = await fetch("https://ipinfo.io/json?token=9f9dee509d49e4");
-        const data = await res.json();
-        callback(data.country);
-      } catch (e) {
-        callback("ua");
-      }
-    },
-    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
-  });
-
-  // Після ініціалізації телефону
-  document.querySelectorAll(".iti__country-name").forEach(el => {
-    el.textContent = el.textContent.replace(/\s*\(.*?\)/, "");
-  });
-}
-
 $(document).ready(function () {
   $(".reviews-carousel").slick({
       infinite: true,
