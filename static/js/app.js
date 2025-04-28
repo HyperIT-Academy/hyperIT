abde0f9a17ae4f42dd013b6eb96458658f61fbf3
HyperIT-Academy <hyperitacademy@gmail.com>
2025-04-28 22:53:01 +0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -22,33 +22,4 @@ $(document).ready(function () {
       $answer.toggleClass("hidden");
       $(this).toggleClass("rotate");
   });
-
-  const $input = $(".hero-input-phone");
-
-  $input.intlTelInput({
-    initialCountry: "auto",
-    locale: "en",
-    localizedCountries: {
-      cg: "Congo - Brazzaville",
-      cd: "Congo - Kinshasa"
-    },
-    nationalMode: true,
-    autoPlaceholder: "polite",
-    formatOnDisplay: false,
-    separateDialCode: false, 
-    geoIpLookup: function (callback) {
-      fetch("https://ipinfo.io/json?token=9f9dee509d49e4")
-        .then(res => res.json())
-        .then(data => callback(data.country))
-        .catch(() => callback("ua"));
-    },
-    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
-  });
-
-
-  setTimeout(() => {
-      document.querySelectorAll(".iti__country-name").forEach(el => {
-        el.textContent = el.textContent.replace(/\s*\(.*?\)/, "");
-      });
-  }, 500);
 });
