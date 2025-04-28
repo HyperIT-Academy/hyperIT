96fa86736662eaa33fb4c09f7f1eb058da786cd0
HyperIT-Academy <hyperitacademy@gmail.com>
2025-04-28 13:08:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.post('/api/send', async (req, res) => {
   const { name, phone, email } = req.body;
 
   try {
-    const response = await axios.post('https://api.binotel.com/api/4.0/smartcrm/createDeal', {
+    const response = await axios.post('https://api.binotel.com/api/4.0/crm/deal/create', {
       key: SMARTCRM_KEY,
       secret: SMARTCRM_SECRET,
       pipelineId: 6046,
