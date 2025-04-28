31f9c237fcd55df43044199f83680c043fbeae3d
HyperIT-Academy <hyperitacademy@gmail.com>
2025-04-28 18:52:33 +0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,4 @@
-$(document).ready(function () {
-  $(".reviews-carousel").slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [
-          {
-              breakpoint: 768,
-              settings: {
-                  arrows: true,
-                  slidesToShow: 1,
-                  centerMode: true,
-                  centerPadding: '50px'
-              },
-          }
-      ],
-  });
-
-
-  $(".faq-btn-js").on("click", function () {
-      var $answer = $(this).closest("li").find(".faq__answer");
-      $answer.toggleClass("hidden");
-      $(this).toggleClass("rotate");
-  });
-
-  export function initPhoneInput() {
+ export function initPhoneInput() {
     const $input = $(".hero-input-phone");
 
   $input.intlTelInput({
@@ -53,4 +28,29 @@ $(document).ready(function () {
       });
   }, 500);
   }
+
+$(document).ready(function () {
+  $(".reviews-carousel").slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  arrows: true,
+                  slidesToShow: 1,
+                  centerMode: true,
+                  centerPadding: '50px'
+              },
+          }
+      ],
+  });
+
+
+  $(".faq-btn-js").on("click", function () {
+      var $answer = $(this).closest("li").find(".faq__answer");
+      $answer.toggleClass("hidden");
+      $(this).toggleClass("rotate");
+  });
 });
